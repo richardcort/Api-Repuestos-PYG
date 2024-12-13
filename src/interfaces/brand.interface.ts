cc202dd220b6d61731909b5f3ba40c784cd834fa
Richard <richardcortez287@gmail.com>
2024-12-13 15:42:51 -0400

    Create brand interface

---
+++ src/interfaces/brand.interface.ts
@@ -0,0 +1,8 @@
+export interface BrandInterface {
+    brand_code?: number
+    name: string
+    status?: boolean
+    createdAt: Date
+    updatedAt: Date
+    deletedAt: Date
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
+export * from './brand.interface'
 export * from './company.interface'
 export * from './role.interface'
 export * from './user.interface'
