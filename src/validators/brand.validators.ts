af18c66b7ae586fdf30dca34e01676ca61a83e09
Richard <richardcortez287@gmail.com>
2024-12-19 15:51:09 -0400

    Modify somethings in brand validator

--- src/validators/brand.validators.ts
+++ src/validators/brand.validators.ts
@@ -4,32 +4,56 @@ import { brandServices } from "../services"
 
 class BrandValidator {
     public validateBrand = [
-        body("brandCode").notEmpty().withMessage("Brand Code is required"),
-        body("brandCode").isString().withMessage("Brand Code must be string"),
-        body("brandCode").isLength({ min: 3, max: 10 }).withMessage("Brand Code must be between 3 and 10 characters"),
+        body("brand_code").notEmpty().withMessage("Brand Code is required"),
+        body("brand_code").isString().withMessage("Brand Code must be string"),
+        body("brand_code").isLength({ min: 3, max: 10 }).withMessage("Brand Code must be between 3 and 10 characters"),
         body("name").notEmpty().withMessage("Name is required"),
         body("name").isString().withMessage("Name must be string"),
         body("name").isLength({ min: 3, max: 50 }).withMessage("Name must be between 3 and 50 characters"),
     ]
 
     public async checkBrandCode(req: Request, res: Response, next: NextFunction) {
-        const { brandCode } = req.body
-        const { status, message } = await brandServices.getOne(brandCode as string)
-        if (status === 500) {
-            return res.status(status).json({
-                message
-            })
-        } else if (status === 200) {
-            return res.status(409).json({
-                errors: [
-                    {
-                        type: "field",
-                        msg: `Brand Code is already in use`,
-                        location: "body"
-                    }
-                ]
-            })
-        } 
+        const { code } = req.params
+        const { brand_code } = req.body
+        if (code) {
+            const { status, message } = await brandServices.getOne(code)
+            if (status === 500) {
+                return res.status(status).json({
+                    message
+                })
+            } else if (status === 404) {
+                return res.status(409).json({
+                    errors: [
+                        {
+                            type: "field",
+                            value: code,
+                            msg: `Brand Code not found`,
+                            path: "code",
+                            location: "params"
+                        }
+                    ]
+                })
+            }
+        } else {
+            const { status, message } = await brandServices.getOne(brand_code as string)
+            if (status === 500) {
+                return res.status(status).json({
+                    message
+                })
+            } else if (status === 200) {
+                return res.status(409).json({
+                    errors: [
+                        {
+                            type: "field",
+                            value: "",
+                            msg: `Brand Code is already in use`,
+                            path: "brand_code",
+                            location: "body"
+                        }
+                    ]
+                })
+            }
+        }
         next()
     }
 
@@ -45,12 +69,14 @@ class BrandValidator {
                 errors: [
                     {
                         type: "field",
+                        value: "",
                         msg: `Name is already in use`,
+                        path: "name",
                         location: "body"
                     }
                 ]
             })
-        } 
+        }
         next()
     }
 }
